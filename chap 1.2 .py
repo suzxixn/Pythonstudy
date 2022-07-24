e9805f9147b9f388e45ab79a81d01d4eaaa23ba5
suzxixn <109528541+suzxixn@users.noreply.github.com>
2022-07-24 23:07:41 +0900

    Add files via upload

---
+++ chap 1.2 .py
@@ -0,0 +1,18 @@
+Python 3.10.5 (v3.10.5:f377153967, Jun  6 2022, 12:36:10) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+watch = 100000
+lighter = 300
+watch + lighter
+100300
+1000000 * 0.85
+850000.0
+price = 5000
+price *= 0.85
+price
+4250.0
+a = 'pig'
+b = 'dad'
+a + b
+'pigdad'
+a + ' ' +b
+'pig dad'
